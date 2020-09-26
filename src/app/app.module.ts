d6c1a1f9cc6aaa6eff6331d60f2ec1d905335fb9
ConstLaf <developer.constance@gmail.com>
2020-09-26 13:02:00 -0300

    Adds angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+/*Styles imports */
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -16,6 +20,7 @@ import { FiltersComponent } from './components/filters/filters.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
 import { TipsComponent } from './components/tips/tips.component';
 import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
+
 
 @NgModule({
   declarations: [
@@ -34,7 +39,10 @@ import { RecipeDetailsComponent } from './components/recipe-details/recipe-detai
     AppRoutingModule,
     NgbModule,
     AngularFirestoreModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
