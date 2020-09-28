0db8ab1d33d8681367d3f4a11b9acedf072462c4
kthcsk <csk.kath@gmail.com>
2020-09-28 18:26:09 -0300

    header update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatSliderModule } from '@angular/material/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -52,6 +57,11 @@ import { AngularfireConectionService } from './services/angularfire-conection.se
     BrowserAnimationsModule,
     MatSliderModule,
     MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    FlexLayoutModule,
     HttpClientModule,
   ],
   providers: [RecipesdataService, AngularfireConectionService],
